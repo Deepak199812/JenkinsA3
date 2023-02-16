63bc7f57998ae427c8c9d79b0d50fe34324e6700
deepak199812 <deepak19981226@gmail.com>
2023-02-16 17:08:03 +0000

    Final commit of Assignment3 Q1

--- A3.java
+++ A3.java
@@ -2,6 +2,6 @@ public class A3
 {
        public static void main(String[] args)
        {
-         System.out.println("Hello");
+         System.out.println("Hello! Hey hi once again devops team hope everything going good");
        }
 }
